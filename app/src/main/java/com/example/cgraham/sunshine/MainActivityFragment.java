d488f4f9520d6a92414d2e4e69f67acbc190f34b
Chad Graham <cgraham@profireenergy.com>
2015-06-10 18:07:09 -0600

    Added string array for forecast. Added ArrayAdapter for forecast.

--- app/src/main/java/com/example/cgraham/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/cgraham/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 
 /**
@@ -12,12 +17,27 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    ArrayAdapter<String> m_ForecastAdapter;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] MyStringArray = {  "Today - Sunny - 28/18",
+                                    "Tomorrow - Rain - 23/15",
+                                    "Friday - Sunny - 25/13",
+                                    "Saturday - Sunny - 26/19",
+                                    "Sunday - Sunny - 25/16",
+                                    "Monday - Sunny - 25/18"};
+        List<String> forecast = new ArrayList<>(Arrays.asList(MyStringArray));
+        m_ForecastAdapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecast
+        );
+        return rootView;
     }
 }
